001405e9b9d6b355b7dc3ca41ffc340b677b6c95
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-14 14:46:16 +0700

    commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 // Responsible for the business logic (without connecting database)
 
 @Injectable({})
 export class AuthService {
+   constructor(private  prisma: PrismaService) {}
    signup() {
       return { msg: 'I have signed up' };
    }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
 
 // Write logic connect database
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+   constructor() {
+      super({
+         datasources: {
+            db: {
+               url: 'postgresql://postgres:tptd1234560@localhost:5433/nest?schema=public'
+            }
+         }
+      })
+   }
+}
