2972b09e530cebb8f1d18a2b84e11c2626a89633
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-15 22:44:54 +0700

    Create signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,10 +10,7 @@ export class AuthController{
    // [POST] /auth/signup
    @Post('signup')
    signup(@Body() dto: AuthDto) {
-      console.log({
-         dto,
-      })
-      return this.authService.signup()
+      return this.authService.signup(dto)
    }
 
    // [POST] /auth/signin   
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true, // Do not accept fields not in dto
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
