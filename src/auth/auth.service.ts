ccb87860c1106a5cfcc69235027b96340f6c45c3
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-18 10:34:23 +0700

    Create strategy folder

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -65,17 +65,20 @@ export class AuthService {
       return this.signToken(user.id, user.email);
    }
 
-   signToken(userId: number, email: string): Promise<string> {
+   async signToken(userId: number, email: string): Promise<{access_token:string}> {
       const payload = {
          sub: userId,
          email
       };
-      const secret = this.config.get('JWT_ SECRET')
+      const secret = this.config.get('JWT_SECRET')
 
-      return this.jwt.signAsync(payload, {
+      const token = await this.jwt.signAsync(payload, {
          expiresIn: '15m',
          secret: secret,
       });
+      return {
+         access_token: token,
+      };
    }
 
 }
